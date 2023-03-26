2e7a6ddb95ab22892414f8b65d5cb8d5d76b4354
edmccrea <ed-mccrea@hotmail.co.uk>
2023-03-26 22:36:16 +0200

    feat: add vertical and horizontal grid components

---
+++ src/lib/components/HorizontalGrid.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let images;
+</script>
+
+<div class="container">
+  <ul class="image-gallery">
+    {#each images as image}
+      <li>
+        <img src={image.url} alt="" />
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .image-gallery {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .image-gallery > li {
+    height: 300px;
+    cursor: pointer;
+    position: relative;
+    flex: 1 1 auto;
+  }
+
+  .image-gallery li img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    vertical-align: middle;
+    border-radius: 5px;
+  }
+
+  .image-gallery::after {
+    content: "";
+    flex-grow: 999;
+  }
+</style>
---
+++ src/lib/components/VerticalGrid.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  export let images;
+  const columns = images.reduce((acc, image, index) => {
+    const columnIndex = index % 3;
+    if (!acc[columnIndex]) {
+      acc[columnIndex] = { images: [] };
+    }
+    acc[columnIndex].images.push(image);
+    return acc;
+  }, []);
+</script>
+
+<div class="image-gallery" in:fade>
+  {#each columns as column}
+    <div class="column">
+      {#each column.images as image}
+        <div class="image-item">
+          <img src={image.url} alt="" />
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .image-gallery {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .image-gallery .column {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 33%;
+  }
+
+  .image-item img {
+    width: 100%;
+    border-radius: 5px;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .image-gallery {
+      flex-direction: row;
+    }
+  }
+</style>
