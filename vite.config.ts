d55d2d6bb2addc6432397601acd4f73fe23c8ccd
edmccrea <ed-mccrea@hotmail.co.uk>
2023-04-07 22:30:15 +0200

    feat: web vitals test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  define: {
+    "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(
+      process.env.VERCEL_ANALYTICS_ID
+    ),
+  },
+};
+
+export default config;
