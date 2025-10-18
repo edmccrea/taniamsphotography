a6dfc15a9e56baa6a4215741ebe0acab04fd44ca
edmccrea <ed-mccrea@hotmail.co.uk>
2025-10-18 11:58:34 +0200

    node 22

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x',
-		}),
-	}
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs22.x',
+    }),
+  },
 };
 
 export default config;
