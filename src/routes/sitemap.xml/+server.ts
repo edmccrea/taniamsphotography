b46b80b98648c5e76562320dba2529c49292f98b
edmccrea <ed-mccrea@hotmail.co.uk>
2025-12-06 21:32:45 +0100

    update sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,13 +1,47 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { DATO_API_KEY } from '$env/static/private';
+
 export async function GET() {
   const site = 'https://www.taniamccreasteele.com';
-  const pages = [
+  const staticPages = [
     '',
     '/about',
-    '/gallery',
+    '/collections',
     '/shop',
     '/contact',
     '/blog'
   ];
+
+  const query = gql`
+    {
+      allGalleryCollections {
+        url
+      }
+      allBlogPosts {
+        url
+      }
+    }
+  `;
+
+  const graphQLClient = new GraphQLClient('https://graphql.datocms.com/', {
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${DATO_API_KEY}`,
+    },
+  });
+
+  const data: {
+    allGalleryCollections: { url: string }[];
+    allBlogPosts: { url: string }[];
+  } = await graphQLClient.request(query);
+
+  const galleryPages = data.allGalleryCollections.map(
+    (gallery) => `/gallery/${gallery.url}`
+  );
+
+  const blogPages = data.allBlogPosts.map((post) => `/blog/${post.url}`);
+
+  const pages = [...staticPages, ...galleryPages, ...blogPages];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
 <urlset
